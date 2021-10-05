93bc4391d2cd55dc19b82d080dade46c6e804e6e
Juan Carlos Carvajal <juanc.carvajal72@gmail.com>
2021-10-05 18:31:09 -0500

    Create Migrations

---
+++ MascotaFeliz.App.Persistencia/Migrations/20211005233029_Inicial.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MascotaFeliz.App.Persistencia.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Personas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Apellidos = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumeroTelefono = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CorreoElectronico = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Usuario = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Contrasenia = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TipoUsuario = table.Column<int>(type: "int", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PersonaId = table.Column<int>(type: "int", nullable: true),
+                    Identificacion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Propietario_PersonaId = table.Column<int>(type: "int", nullable: true),
+                    TarjetaProfesional = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Activo = table.Column<bool>(type: "bit", nullable: true),
+                    Veterinario_PersonaId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Personas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mascotas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TipoMascota = table.Column<int>(type: "int", nullable: false),
+                    Raza = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Genero = table.Column<int>(type: "int", nullable: false),
+                    AnioNacimiento = table.Column<int>(type: "int", nullable: false),
+                    PropietarioId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mascotas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Mascotas_Personas_PropietarioId",
+                        column: x => x.PropietarioId,
+                        principalTable: "Personas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConsultasDomiciliarias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Hora = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Temperatura = table.Column<float>(type: "real", nullable: false),
+                    Peso = table.Column<float>(type: "real", nullable: false),
+                    FrecuenciaRespiratoria = table.Column<float>(type: "real", nullable: false),
+                    FrecuenciaCardiaca = table.Column<float>(type: "real", nullable: false),
+                    EstadoAnimo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Diagnostico = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MascotaId = table.Column<int>(type: "int", nullable: true),
+                    VeterinarioId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConsultasDomiciliarias", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ConsultasDomiciliarias_Mascotas_MascotaId",
+                        column: x => x.MascotaId,
+                        principalTable: "Mascotas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ConsultasDomiciliarias_Personas_VeterinarioId",
+                        column: x => x.VeterinarioId,
+                        principalTable: "Personas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConsultasDomiciliarias_MascotaId",
+                table: "ConsultasDomiciliarias",
+                column: "MascotaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConsultasDomiciliarias_VeterinarioId",
+                table: "ConsultasDomiciliarias",
+                column: "VeterinarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mascotas_PropietarioId",
+                table: "Mascotas",
+                column: "PropietarioId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ConsultasDomiciliarias");
+
+            migrationBuilder.DropTable(
+                name: "Mascotas");
+
+            migrationBuilder.DropTable(
+                name: "Personas");
+        }
+    }
+}
