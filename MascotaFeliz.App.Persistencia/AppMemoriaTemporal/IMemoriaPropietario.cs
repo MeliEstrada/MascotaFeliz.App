5e81ad295b47108a303e308f374bae4613cdcc42
Juan Carlos Carvajal <juanc.carvajal72@gmail.com>
2021-10-05 19:43:32 -0500

    Create Interface IMemoriaPropietarios

---
+++ MascotaFeliz.App.Persistencia/AppMemoriaTemporal/IMemoriaPropietario.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using MascotaFeliz.App.Dominio;
+
+namespace MascotaFeliz.App.Persistencia.AppMemoriaTemporal
+{
+    public interface IMemoriaPropietario
+    {
+         IEnumerable<Propietario> GetAll();
+    }
+}
