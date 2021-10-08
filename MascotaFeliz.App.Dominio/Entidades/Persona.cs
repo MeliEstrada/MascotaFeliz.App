1793b75f48f12fac4dd18a802f975f52a9510573
Juan Carlos Carvajal <juanc.carvajal72@gmail.com>
2021-10-08 15:09:12 -0500

    Validation in Dominio

--- MascotaFeliz.App.Dominio/Entidades/Persona.cs
+++ MascotaFeliz.App.Dominio/Entidades/Persona.cs
@@ -1,14 +1,22 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace MascotaFeliz.App.Dominio
 {
     public class Persona
     {
         public int Id {get;set;}
+        [Required, StringLength(50)]
         public string Nombre {get;set;}
+        [Required, StringLength(50)]
         public string Apellidos {get;set;}
+        [Required, StringLength(20)]
         public string NumeroTelefono {get;set;}
+        [Required, StringLength(50)]
         public string CorreoElectronico {get;set;}
+        // [Required, StringLength(20)]
         public string Usuario {get;set;}
+        // [Required, StringLength(20)]
         public string Contrasenia {get;set;}
         public TipoUsuario TipoUsuario {get;set;}
     }
