8b7cdf21c8a0c7a6ed369d0a2e8db036642b5d2f
Juan Carlos Carvajal <juanc.carvajal72@gmail.com>
2021-10-08 12:05:39 -0500

    Interface for Propietario in AppMemoriaTemporal

--- MascotaFeliz.App.Persistencia/AppMemoriaTemporal/MemoriaPropietario.cs
+++ MascotaFeliz.App.Persistencia/AppMemoriaTemporal/MemoriaPropietario.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using MascotaFeliz.App.Dominio;
 
 namespace MascotaFeliz.App.Persistencia.AppMemoriaTemporal
@@ -20,18 +22,69 @@ namespace MascotaFeliz.App.Persistencia.AppMemoriaTemporal
                 new Propietario{Id=3, Nombre="Juan Carlos",
                     Apellidos="Carvajal Tapias", NumeroTelefono="3158556483",
                     CorreoElectronico="juanc.carvajal72@gmail.com"},
-                new Propietario{Id=3, Nombre="Melissa Andrea",
+                new Propietario{Id=4, Nombre="Melissa Andrea",
                     Apellidos="Estrada Marín", NumeroTelefono="3004610198",
                     CorreoElectronico="andreita_9815@hotmail.com"},
-                new Propietario{Id=3, Nombre="Robin",
+                new Propietario{Id=5, Nombre="Robin",
                     Apellidos="Benítez Mora", NumeroTelefono="3008060020",
                     CorreoElectronico="robinbenitez@yahoo.com"}
             };
         }
 
-        public IEnumerable<Propietario> GetAll()
+        public Propietario AddPropietario(Propietario nuevoPropietario)
+        {
+            nuevoPropietario.Id = propietarios.Max(p => p.Id) + 1;
+            propietarios.Add(nuevoPropietario);
+            return nuevoPropietario;
+        }
+
+        public void DeletePropietario(int propietarioId)
+        {
+            var propietarioEncontrado = GetPropietario(propietarioId);
+            if (propietarioEncontrado != null)
+                propietarios.Remove(propietarioEncontrado);
+        }
+
+        public IEnumerable<Propietario> GetAllPropietarios()
         {
             return propietarios;
         }
+
+        public Propietario GetPropietario(int propietarioId)
+        {
+            return propietarios.SingleOrDefault(p => p.Id == propietarioId);
+        }
+
+        public IEnumerable<Propietario> GetPropietariosPorFiltro(
+            string filtro=null)
+        // La asignación filtro=null indica que el parámetro filtro es opcional
+        {
+            var propietarios = GetAllPropietarios(); // Todos los propietarios
+            if (propietarios != null) // Si se tienen propietarios
+            {
+                // Si el filtro tiene algun valor
+                if (!String.IsNullOrEmpty(filtro))
+                {
+                    propietarios = propietarios.Where(
+                        p => (p.Nombre + " " + p.Apellidos).Contains(filtro));
+                    // Filtra los propietarios que contienen el filtro
+                }
+            }
+            return propietarios;
+        }
+
+        public Propietario UpdatePropietario(Propietario propietarioActualizado)
+        {
+            var propietario = GetPropietario(propietarioActualizado.Id);
+            if (propietario != null)
+            {
+                propietario.Nombre = propietarioActualizado.Nombre;
+                propietario.Apellidos = propietarioActualizado.Apellidos;
+                propietario.NumeroTelefono = propietarioActualizado.NumeroTelefono;
+                propietario.CorreoElectronico = propietarioActualizado.CorreoElectronico;
+            }
+            return propietario;
+        }
+        
     }
 }
