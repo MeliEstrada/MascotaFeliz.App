2b9b301c16b3f99c2c00668217c68f67a6627f5e
Juan Carlos Carvajal <juanc.carvajal72@gmail.com>
2021-10-08 13:52:49 -0500

    EditPropietarioProvisional

---
+++ MascotaFeliz.App.Frontend/Pages/Propietario/EditPropietarioProvisional.cshtml.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MascotaFeliz.App.Dominio;
+using MascotaFeliz.App.Persistencia.AppMemoriaTemporal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MascotaFeliz.App.Frontend.Pages
+{
+    public class EditPropietarioProvisionalModel : PageModel
+    {
+        private readonly IMemoriaPropietario memoriaPropietario;
+        [BindProperty]
+        public Propietario Propietario {get;set;}
+
+        public EditPropietarioProvisionalModel(
+            IMemoriaPropietario memoriaPropietario)
+        {
+            this.memoriaPropietario = memoriaPropietario;
+        }
+
+        public IActionResult OnGet(int? propietarioId)
+        {
+            if (!propietarioId.HasValue) Propietario = new Propietario();
+            else Propietario =
+                memoriaPropietario.GetPropietario(propietarioId.Value);
+            if (Propietario == null) return RedirectToPage("./NotFound");
+            else return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid) return Page();
+            if (Propietario.Id > 0)
+            {
+                Propietario = memoriaPropietario.UpdatePropietario(Propietario);
+            }
+            else
+            {
+                Propietario = memoriaPropietario.AddPropietario(Propietario);
+            }
+            return Page();
+        }
+
+    }
+}
