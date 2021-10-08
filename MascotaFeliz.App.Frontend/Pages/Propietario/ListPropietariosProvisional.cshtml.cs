3ace8eb2a80ea42968ef1e99ddc95037caeb03c9
Juan Carlos Carvajal <juanc.carvajal72@gmail.com>
2021-10-08 13:20:11 -0500

    ListPropietariosProvisional

--- MascotaFeliz.App.Frontend/Pages/Propietario/ListPropietariosProvisional.cshtml.cs
+++ MascotaFeliz.App.Frontend/Pages/Propietario/ListPropietariosProvisional.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MascotaFeliz.App.Dominio;
+using MascotaFeliz.App.Persistencia.AppMemoriaTemporal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,23 @@ namespace MascotaFeliz.App.Frontend.Pages
 {
     public class ListPropietariosProvisionalModel : PageModel
     {
-        public void OnGet()
+        private readonly IMemoriaPropietario memoriaPropietario;
+        public IEnumerable<Propietario> Propietarios {get;set;}
+        [BindProperty(SupportsGet = true)]
+        public string FiltroBusqueda {get;set;}
+
+        public ListPropietariosProvisionalModel(
+            IMemoriaPropietario memoriaPropietario)
         {
+            this.memoriaPropietario = memoriaPropietario;
         }
+
+        public void OnGet(string filtroBusqueda)
+        {
+            FiltroBusqueda = filtroBusqueda;
+            Propietarios = memoriaPropietario.GetPropietariosPorFiltro(
+                filtroBusqueda);
+        }
+
     }
 }
