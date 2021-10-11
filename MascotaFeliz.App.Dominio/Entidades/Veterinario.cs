dde52584061ca173a13cb11cc07c991ffb82739d
Melissa Estrada <melissa.30614256623@ucaldas.edu.co>
2021-10-10 22:26:44 -0500

    Modificación Entidad Veterinario

--- MascotaFeliz.App.Dominio/Entidades/Veterinario.cs
+++ MascotaFeliz.App.Dominio/Entidades/Veterinario.cs
@@ -3,6 +3,7 @@ namespace MascotaFeliz.App.Dominio
 {
     public class Veterinario : Persona
     {
+        [Required, StringLength(5)]
         public string TarjetaProfesional {get;set;}
         public Boolean Activo {get;set;}
         public int PersonaId {get;set;}
