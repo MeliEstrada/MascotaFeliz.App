007c66c94807766c63686b2a593406e29531138d
Andre <andrea.solarte72@gmail.com>
2021-10-11 10:51:35 -0500

    List Mascotas Provisional

--- MascotaFeliz.App.Frontend/Pages/Mascota/ListMascotasProvisional.cshtml.cs
+++ MascotaFeliz.App.Frontend/Pages/Mascota/ListMascotasProvisional.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MascotaFeliz.App.Dominio;
+using MascotaFeliz.App.Persistencia.AppMemoriaTemporal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,23 @@ namespace MascotaFeliz.App.Frontend.Pages
 {
     public class ListMascotasProvisionalModel : PageModel
     {
-        public void OnGet()
+        private readonly IMemoriaMascota memoriaMascota;
+        public IEnumerable<Mascota> Mascotas {get;set;}
+        [BindProperty(SupportsGet = true)]
+        public string FiltroBusqueda {get;set;}
+
+        public ListMascotasProvisionalModel(
+            IMemoriaMascota memoriaMascota)
         {
+            this.memoriaMascota = memoriaMascota;
+        }
+
+        public void OnGet(string filtroBusqueda)
+        {
+            FiltroBusqueda = filtroBusqueda;
+                Mascotas = memoriaMascota.GetMascotasPorFiltro(
+                filtroBusqueda);
+
         }
     }
 }
