2e853d6df207b9a3b4aefbd496b39bea184e9ef9
Andre <andrea.solarte72@gmail.com>
2021-10-11 10:37:06 -0500

    Implementación de repositorio memoria mascota en persistencia

---
+++ MascotaFeliz.App.Persistencia/AppMemoriaTemporal/IMemoriaMascota.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using MascotaFeliz.App.Dominio;
+
+namespace MascotaFeliz.App.Persistencia.AppMemoriaTemporal
+{
+    
+    public interface IMemoriaMascota
+    {
+        IEnumerable<Mascota> GetAllMascotas();
+        IEnumerable<Mascota> GetMascotasPorFiltro(string filtro);
+        Mascota GetMascota(int mascotaId);
+        Mascota UpdateMascota(Mascota mascotaActualizada);
+        Mascota AddMascota(Mascota nuevaMascota);
+        void DeleteMascota(int MascotaId);   
+    }
+ }
