0bbf0a99e864930c310611a81498650cc1001157
Juan Carlos Carvajal <juanc.carvajal72@gmail.com>
2021-10-14 01:28:25 -0500

    Adjustments in namespace AppRepositorios

--- MascotaFeliz.App.Persistencia/AppRepositorios/RepositorioAdministrador.cs
+++ MascotaFeliz.App.Persistencia/AppRepositorios/RepositorioAdministrador.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using MascotaFeliz.App.Dominio;
 
-namespace MascotaFeliz.App.Persistencia
+namespace MascotaFeliz.App.Persistencia.AppRepositorios
 {
     public class RepositorioAdministrador : IRepositorioAdministrador
     {
